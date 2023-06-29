52174f269f3d9af412df8ba3f3c5cb8e78bbc483
raihanmd <mraihanf471@gmail.com>
2023-06-29 20:14:52 +0700

    'update'

--- src/middleware.js
+++ src/middleware.js
@@ -3,14 +3,15 @@ import { myResponse } from "./utils/myResponse";
 
 const authMiddleware = ["/api/login", "/api/cart", "/api/checkout", "/api/order", "/api/products", "/api/wishlist", "/api/wallet"];
 
-// const allowedOrigins = process.env.NODE_ENV === "production" ? ["https://www.ecomerce.raihanmd.site", "https://ecomerce.raihanmd.site"] : ["http://localhost:3000"];
+const allowedOrigins = process.env.NODE_ENV === "production" ? ["https://www.ecomerce.raihanmd.site", "https://ecomerce.raihanmd.site", "https://next-auth.js.org"] : ["http://localhost:3000"];
 
 export async function middleware(request) {
   const origin = request.headers.get("origin");
 
-  // if (origin && !allowedOrigins.includes(origin)) {
-  //   return myResponse(400, "Bad request.", "Bad request.");
-  // }
+  if (origin && !allowedOrigins.includes(origin)) {
+    console.log(origin);
+    return myResponse(400, "Bad request.", "Bad request.");
+  }
 
   if (request.nextUrl.pathname.startsWith("/api") && !request.nextUrl.pathname.startsWith("/api/auth") && request.method === "POST") {
     if (request.headers.get("API-Key") !== process.env.API_KEY) {
