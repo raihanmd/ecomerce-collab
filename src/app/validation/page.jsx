1ad6f2e21fc772711a43f9ed3fc9611c7e54f265
raihanmd <mraihanf471@gmail.com>
2023-09-09 17:36:30 +0700

    'update'

--- src/app/validation/page.jsx
+++ src/app/validation/page.jsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { useState, useEffect } from "react";
 import { redirect } from "next/navigation";
-import { Flex, Box, FormControl, FormLabel, Stack, Button, Text, useToast, Alert, AlertIcon, AlertTitle } from "@chakra-ui/react";
-import { AutoComplete, AutoCompleteInput, AutoCompleteItem, AutoCompleteList } from "@choc-ui/chakra-autocomplete";
+import { Stack, Text, FormControl, FormLabel, Select, Button, Flex, Box, useToast, Alert, AlertIcon, AlertTitle } from "@chakra-ui/react";
 
 import { useUserContext } from "@/context/UserContext";
 import { fetchGET } from "@/useFetch/fetchGET";
@@ -18,20 +17,13 @@ export default function page() {
 
   const [provinces, setProvinces] = useState([]);
   const [cities, setCities] = useState([]);
-  const [selectedProvince, setSelectedProvince] = useState(null);
-  const [selectedCity, setSelectedCity] = useState("");
 
-  const { register, handleSubmit, setValue } = useForm();
+  const [selectedProvince, setSelectedProvince] = useState(null);
+  const [selectedCity, setSelectedCity] = useState(null);
+
+  const { register, handleSubmit } = useForm();
   const [isError, setIsError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-
-  const handleProvince = (province) => {
-    setValue("province", province);
-  };
-
-  const handleCity = (city) => {
-    setValue("city", city);
-  };
 
   useEffect(() => {
     const fetchProvinces = async () => {
@@ -39,38 +31,75 @@ export default function page() {
         const { payload } = await fetchGET("/api/rajaongkir/province", { component: "client" });
         setProvinces(payload);
       } catch (error) {
-        console.log(error);
+        toast({
+          title: "Something went wrong.",
+          position: "top-right",
+          status: "error",
+          isClosable: true,
+        });
       }
     };
     fetchProvinces();
   }, []);
 
   useEffect(() => {
-    if (!selectedProvince) {
-      return;
-    }
+    if (!selectedProvince) return;
 
     const fetchCities = async () => {
       try {
         const { payload } = await fetchGET(`/api/rajaongkir/city/${selectedProvince}`, { component: "client" });
         setCities(payload);
       } catch (error) {
-        console.error("Error fetching cities:", error);
+        toast({
+          title: "Something went wrong.",
+          position: "top-right",
+          status: "error",
+          isClosable: true,
+        });
       }
     };
 
     fetchCities();
   }, [selectedProvince]);
 
+  const handleProvinceChange = async (e) => {
+    const selectedProvinceId = e.target.value;
+
+    setSelectedProvince(selectedProvinceId);
+    setSelectedCity("");
+
+    if (selectedProvinceId) {
+      try {
+        const { payload } = await fetchGET(`/api/rajaongkir/city/${selectedProvinceId}`, { component: "client" });
+        setCities(payload);
+      } catch (error) {
+        toast({
+          title: "Something went wrong.",
+          position: "top-right",
+          status: "error",
+          isClosable: true,
+        });
+        setIsError(true);
+      }
+    } else {
+      setCities([]);
+    }
+  };
+
   const onSubmitValidation = async (data) => {
     try {
       setIsLoading(true);
+
+      const formData = {
+        ...data,
+      };
+      console.log(formData);
       console.log(data);
 
       // return (window.location.href = "/");
     } catch (err) {
       toast({
-        title: "Product added failed.",
+        title: "Something went srong.",
         position: "top-right",
         status: "error",
         isClosable: true,
@@ -95,30 +124,24 @@ export default function page() {
               <Text>Warning, do not fill it with real data identity, fill it with dummy data</Text>
               <FormControl id="province" isRequired>
                 <FormLabel>Your Province</FormLabel>
-                <AutoComplete openOnFocus>
-                  <AutoCompleteInput variant="filled" />
-                  <AutoCompleteList>
-                    {provinces.map((province) => (
-                      <AutoCompleteItem key={`option-${province.province_id}`} value={province.province_id} textTransform="capitalize" align="center">
-                        <Text ml="4">{province.province}</Text>
-                      </AutoCompleteItem>
-                    ))}
-                  </AutoCompleteList>
-                </AutoComplete>
+                <Select {...register("province")} placeholder="Select Your Province" onChange={handleProvinceChange} value={selectedProvince} onActive={{ borderColor: "black" }}>
+                  {provinces.map((province) => (
+                    <option key={`province-option-${province.province_id}`} value={province.province_id}>
+                      {province.province}
+                    </option>
+                  ))}
+                </Select>
               </FormControl>
               {selectedProvince && (
                 <FormControl id="city" isRequired>
                   <FormLabel>Your City</FormLabel>
-                  <AutoComplete openOnFocus>
-                    <AutoCompleteInput variant="filled" />
-                    <AutoCompleteList>
-                      {cities.map((city) => (
-                        <AutoCompleteItem key={`option-${city.city_id}`} value={city.city_id} textTransform="capitalize" align="center">
-                          <Text ml="4">{city.city}</Text>
-                        </AutoCompleteItem>
-                      ))}
-                    </AutoCompleteList>
-                  </AutoComplete>
+                  <Select {...register("city")} placeholder="Select Your City" value={selectedCity} onActive={{ borderColor: "black" }}>
+                    {cities.map((city) => (
+                      <option key={`city-option-${city.city_id}`} value={city.city_id}>
+                        {city.city_name}
+                      </option>
+                    ))}
+                  </Select>
                 </FormControl>
               )}
 
@@ -134,7 +157,7 @@ export default function page() {
                     bg: "gray.900",
                   }}
                 >
-                  Add Product
+                  Confirm my validation
                 </Button>
               </Stack>
             </Stack>
